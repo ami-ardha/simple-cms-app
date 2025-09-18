7b3925d95d10df43eb9133ace81f653bce69bb28
Amillio Ardha Mulana <ami.ardha.m@gmail.com>
2025-09-18 13:52:11 +0700

    fix: prettier config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,14 @@
-import { dirname } from 'path'
-import { fileURLToPath } from 'url'
-import { FlatCompat } from '@eslint/eslintrc'
-import prettier from 'eslint-config-prettier'
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-config-prettier';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
     baseDirectory: __dirname,
-})
+});
 
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
@@ -20,8 +20,8 @@ const eslintConfig = [
             'build/**',
             'next-env.d.ts',
         ],
-        prettier,
     },
-]
+    prettier,
+];
 
-export default eslintConfig
+export default eslintConfig;
